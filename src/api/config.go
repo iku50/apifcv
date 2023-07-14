a462bd9b08e0105b3eea90ae89b418e04b2d4822
iku50 <wizo.o@outlook.com>
2023-07-15 02:28:50 +0800

    ok

--- src/api/config.go
+++ src/api/config.go
@@ -46,3 +46,83 @@ func GptGet(role, prompt string) string {
 
 	return response.Choices[0].Message.Content
 }
+
+func CVGet(CV string) string {
+	prompt := `现在有一些简历，我使用LayoutXLM算法对它们进行了关键信息提取(Key Instruction Extraction, KIE)，我将给出KIE处理后的信息，您需要将它们转化成标准的、人类可读的json简历格式。您只需要给出经过格式化的json格式即可，千万不要加入其他的文字。以下是您需要参照的json格式：
+    {
+		"name": "",
+    "age": 0,
+    "birthday": "",
+    "tel": "",
+    "email": "",
+    "degree": "",
+    "working_years": 0,
+    "education": [
+        {
+            "begin": 0,
+            "end": 0,
+            "school": "",
+            "degree": "",
+            "major": "",
+            "lessons": ""
+        }
+    ],
+    "work_experience": [
+        {
+            "begin": 0,
+            "end": 0,
+            "company_or_organization": "",
+            "position": ""
+        }
+    ],
+    "school_experience": [
+        {
+            "company_or_organization": "",
+            "position": ""
+        }
+    ],
+    "internship_experience": [
+        {
+            "company_or_organization": "",
+            "position": ""
+        }
+    ],
+    "project_experience": [
+        {
+            "name":"",
+            "desc":""
+        }
+    ],
+    "award":[
+        {
+            "name":"",
+            "level":""
+        }
+    ],
+    "skill":[
+        {
+            "name":""
+        }
+    ],"self_desc":""}
+""}`
+	request := &Request{
+		Model: model,
+		Messages: []*Message{
+			{
+				Role:    "system",
+				Content: prompt,
+			},
+			{
+				Role:    "system",
+				Content: CV,
+			},
+		},
+	}
+
+	response, err := gptApi.Chat(request)
+	if err != nil {
+		panic(err)
+	}
+
+	return response.Choices[0].Message.Content
+}
--- src/main.go
+++ src/main.go
@@ -5,22 +5,44 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/gin-gonic/gin"
+
 	"github.com/iku50/apifcv/src/api"
 )
 
+type CV struct {
+	Content string `json:"content" binding:"required"`
+}
+
 func main() {
-	http.HandleFunc("/", handler)
+	router := gin.Default()
+	router.GET("/gpt", handler)
+	router.POST("/cv", CVHandler)
 	api.Init()
 
-	err := http.ListenAndServe(":12321", nil)
-	if err != nil {
-		log.Fatal("Server error:", err)
+	if err := router.Run(":12321"); err != nil {
+		log.Fatal(err.Error())
 	}
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	user := r.URL.Query().Get("user")
-	prompt := r.URL.Query().Get("prompt")
-	response := api.GptGet(user, prompt)
-	fmt.Fprint(w, response)
+func handler(c *gin.Context) {
+	role := c.Query("role")
+	prompt := c.Query("prompt")
+	fmt.Println(role, prompt)
+	c.JSON(200, gin.H{
+		"message": api.GptGet(role, prompt),
+	})
+}
+
+func CVHandler(c *gin.Context) {
+	var CV CV
+	if err := c.ShouldBindJSON(&CV); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	fmt.Println(CV)
+	cvtext := CV.Content
+	c.JSON(200, gin.H{
+		"message": api.CVGet(cvtext),
+	})
 }
